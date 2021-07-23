035ce87cf152f44539106055836fa5cff1a24864
kameswari <bhagavatula.kameswari@gmail.com>
2021-07-22 21:03:57 -0700

    added TestRunner

---
+++ CucmberPractice/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,9 @@
+package testRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/featureFiles", glue = "stepDefinitions")
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
