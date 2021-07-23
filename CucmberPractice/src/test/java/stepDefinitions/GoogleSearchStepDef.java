9e62523c8432e4caf6d7d1974a346d3ddb6a095a
kameswari <bhagavatula.kameswari@gmail.com>
2021-07-22 21:53:54 -0700

    Test commit

--- CucmberPractice/src/test/java/stepDefinitions/GoogleSearchStepDef.java
+++ CucmberPractice/src/test/java/stepDefinitions/GoogleSearchStepDef.java
@@ -8,16 +8,16 @@ public class GoogleSearchStepDef {
 
 	@Given("I open a browser")
 	public void i_open_a_browser() {
-	  System.out.println("I opened the browser");
+	  System.out.println("I opened the browser.");
 	}
 
 	@When("I navigate to google page")
 	public void i_navigate_to_google_page() {
-	System.out.println("Navigates to google page");
+	System.out.println("Navigates to google page.");
 	}
 
 	@Then("I validate the search text field")
 	public void i_validate_the_search_text_field() {
-	   System.out.println("validate the search text field");
+	   System.out.println("validate the search text field.");
 	}
 }
